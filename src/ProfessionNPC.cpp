3ffa59ad5e2850cb6ad78069c532aac719a17af4
Poszer <41213210+poszer@users.noreply.github.com>
2018-11-15 20:35:53 +0100

    Update ProfessionNPC.cpp
    Disabling fishing profession

--- src/ProfessionNPC.cpp
+++ src/ProfessionNPC.cpp
@@ -205,7 +205,7 @@ public:
 				pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface\\icons\\trade_mining:30|t Mining.", GOSSIP_SENDER_MAIN, 11);
 				pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface\\icons\\spell_holy_sealofsacrifice:30|t First Aid.", GOSSIP_SENDER_MAIN, 12);
 				pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface\\icons\\Inv_misc_food_65:30|t Cooking.", GOSSIP_SENDER_MAIN, 13);
-				pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface\\icons\\Inv_fishingpole_01:30|t Fishing.", GOSSIP_SENDER_MAIN, 14);
+				//pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface\\icons\\Inv_fishingpole_01:30|t Fishing.", GOSSIP_SENDER_MAIN, 14);
 				pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, "|TInterface/ICONS/Thrown_1H_Harpoon_D_01Blue:30|t Nevermind!", GOSSIP_SENDER_MAIN, 15);
 				pPlayer->PlayerTalkClass->SendGossipMenu(1, _creature->GetGUID());
 				break;
@@ -340,16 +340,16 @@ public:
 				CompleteLearnProfession(pPlayer, _creature, SKILL_COOKING);
 				pPlayer->PlayerTalkClass->SendCloseGossip();
 				break;
-			case 14:
-				if (pPlayer->HasSkill(SKILL_FISHING))
-				{
-					pPlayer->PlayerTalkClass->SendCloseGossip();
-					break;
-				}
+			//case 14:
+				//if (pPlayer->HasSkill(SKILL_FISHING))
+				//{
+				//	pPlayer->PlayerTalkClass->SendCloseGossip();
+				//	break;
+				//}
 
-				CompleteLearnProfession(pPlayer, _creature, SKILL_FISHING);
-				pPlayer->PlayerTalkClass->SendCloseGossip();
-				break;
+				//CompleteLearnProfession(pPlayer, _creature, SKILL_FISHING);
+				//pPlayer->PlayerTalkClass->SendCloseGossip();
+				//break;
 			case 15:
 				pPlayer->PlayerTalkClass->SendCloseGossip();
 				break;
